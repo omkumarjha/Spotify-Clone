0997b0c6c26b0b5f64eaa14766098fa04cf7caa6
om <omkumar1892002@gmail.com>
2023-08-05 12:47:09 +0530

    Added indian motivating songs and its cover

---
+++ script.js
@@ -0,0 +1,8 @@
+let audioElement = new Audio("songs/1.mp3");
+
+let element = document.getElementById("masterPlay")
+
+element.addEventListener("click",()=>{
+    console.log("Clicked")
+    // audioElement.play()
+})
